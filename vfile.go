5633cea29738b9350dc651fe1948a29ce213d723
Teages <Teages@teages.xyz>
2022-09-03 13:35:44 +0800

    bug fix & JoinPart

--- vfile.go
+++ vfile.go
@@ -4,7 +4,7 @@ import (
 	"embed"
 	"io/fs"
 	"os"
-	"path"
+	"path/filepath"
 )
 
 var tempPath string
@@ -14,43 +14,50 @@ func init() {
 	os.TempDir()
 }
 
-func Join(filePath string, data []byte) error {
-	dir := GetPath(path.Dir(filePath))
+func Join(fileDir string, data []byte) error {
+	dir := filepath.Dir(GetPath(fileDir))
 	_, err := os.Stat(dir)
 	if err != nil {
 		if !os.IsExist(err) {
 			os.MkdirAll(dir, 0750)
 		}
 	}
-	return os.WriteFile(GetPath(filePath), data, 0644)
+	return os.WriteFile(GetPath(fileDir), data, 0644)
 }
 
-func JoinAll(filePath string, f embed.FS) error {
-	return fs.WalkDir(f, ".", func(p string, d fs.DirEntry, err error) error {
+func JoinAll(fileDir string, f embed.FS) error {
+	return JoinPart(fileDir, ".", f)
+}
+
+func JoinPart(fileDir, partDir string, f embed.FS) error {
+	return fs.WalkDir(f, partDir, func(p string, d fs.DirEntry, err error) error {
 		if !d.IsDir() {
 			fileData, err := f.ReadFile(p)
 			if err != nil {
 				return err
 			}
-			return Join(path.Join(filePath, p), fileData)
+			trueDir, err := filepath.Rel(partDir, p)
+			if err != nil {
+				return err
+			}
+			return Join(filepath.Join(fileDir, trueDir), fileData)
 		}
 		return nil
 	})
 }
-
-func Remove(filePath string) error {
-	fileInfo, err := os.Stat(GetPath(filePath))
+func Remove(fileDir string) error {
+	fileInfo, err := os.Stat(GetPath(fileDir))
 	if err != nil {
 		return err
 	}
 	if fileInfo.IsDir() {
-		os.RemoveAll(GetPath(filePath))
+		os.RemoveAll(GetPath(fileDir))
 	}
 	return nil
 }
 
-func GetPath(filePath string) string {
-	return path.Join(tempPath, filePath)
+func GetPath(fileDir string) string {
+	return filepath.Join(tempPath, fileDir)
 }
 
 func Close() {
